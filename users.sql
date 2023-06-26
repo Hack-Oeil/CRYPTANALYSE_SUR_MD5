6c3a3348593dfe4eee463c4d84c69c8cdbc091e4
Cyrhades <cyrhades76@gmail.com>
2023-06-26 15:39:02 +0200

    Correction, guillemet manquante dans fichier SQL

--- users.sql
+++ users.sql
@@ -2,6 +2,6 @@
 INSERT INTO users (ID, firstname, lastname, username, password, auth_password)
 VALUES
   (1, '-', '-', 'cron', 'd227e7c1f2e95190f865c89f4e8b5c7e', '2'),
-  (2, 'John', 'Doe', 'admin', '5ebe2294ecd0e0f08eab7690d2a6ee69', '1),
+  (2, 'John', 'Doe', 'admin', '5ebe2294ecd0e0f08eab7690d2a6ee69', '1'),
   (3, 'Bob', 'Smith', 'user1', '941f35a0f078e8b843a9827d285678fa', '1'),
   (4, 'Alice', 'Johnson', 'user2', '6253e1406b64bbe6ba7b00ac0bf81257', '1');
